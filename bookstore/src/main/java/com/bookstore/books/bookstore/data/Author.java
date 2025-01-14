45ed56215dfb11aea4388d4016aecfbcc4764754
Mrigraj <119593934+Mrigraj2@users.noreply.github.com>
2025-01-14 13:00:18 +0530

    Add files via upload

---
+++ bookstore/src/main/java/com/bookstore/books/bookstore/data/Author.java
@@ -0,0 +1,24 @@
+package com.bookstore.books.bookstore.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+//import org.springframework.data.annotation.Id;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String birthday;
+
+    // Getters and Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getBirthday() { return birthday; }
+    public void setBirthday(String birthday) { this.birthday = birthday; }
+}
